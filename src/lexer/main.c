d26e527474a8f160593d9306f266596a00067a57
Julie Hazan <hznjulie@gmail.com>
2019-11-13 20:02:18 +0100

    added main and Makefile

---
+++ src/lexer/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "lexer.h"
+
+int main(void)
+{
+    char *str = ", : ; ( )";
+
+    struct lex *l = lexer_alloc(str);
+
+    struct token *token = l->head;
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ", token->type);
+        token = token->next;
+    }
+
+    return 0;
+}
