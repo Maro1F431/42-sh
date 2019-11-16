5217020b204a79b44895de734cbeea22c5950de9
Mathieu Tammaro <mathieu.tammaro@epita.fr>
2019-11-16 14:33:03 +0100

    changed main.c to test if case

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    char str[] = "echo wow";
+    char str[] = "echo wow ; if echo 1 then echo 2 else echo 3 ;";
     struct lex *l = lexer_alloc(str);
     struct ast_node ast;
     ast_node_init(&ast);
@@ -16,6 +16,7 @@ int main()
     print_ast(&ast);
     //char *test = ast.children[0].children[0].data;
     //printf("test %d", ast.children[0].children[0].type);
+    printf("EXEC TIME\n");
     execute(&ast);
     ast_node_free_children(&ast);
     lexer_free(l);
